4c0cbea3f6bbbe81a046fac5d595c310927702e5
Mohammed Shariq Nawaz <mohdshariqnawaz@gmail.com>
2017-09-26 08:52:34 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,166 +1,45 @@
-var myLocations = [
-    {
-        title: 'Discovery Docks Apartments',
-        lat: 51.501409,
-        lng:  -0.018823,
-        type: 'Living Quarters'
-    },
-    {
-        title: 'Nandos Restaurant',
-        lat: 51.5023146,
-        lng: -0.0187593,
-        type: 'Restaurant'
-    },
-    {
-        title: 'Canary Wharf Tube Station',
-        lat: 51.5034898,
-        lng: -0.0185944,
-        type:  'Transit'
-    },
-    {
-        title: 'One Canada Square',
-        lat: 51.5049494,
-        lng: -0.0194997,
-        type:  'Shopping'
-    },
-    {
-        title: 'The O2',
-        lat: 51.503039,
-        lng: 0.003154,
-        type: 'Entertainment'
-    },
-    {
-        title: 'Tesco Express',
-        lat: 51.500575,
-        lng: -0.017354,
-        type: 'Shopping'
-    },
-    {
-        title: 'Firezza Pizza',
-        lat: 51.496268,
-        lng: -0.015511,
-        type: 'Restaurant'
-    },
-    {
-        title: 'The Breakfast Club',
-        lat: 51.506066,
-        lng: -0.017345,
-        type: 'Restaurant'
-    },
-    {
-        title: 'Shake Shack',
-        lat: 51.504905,
-        lng: -0.018967,
-        type: 'Restaurant'
-    },
-    {
-        title: 'Asda Superstore',
-        lat: 51.494206,
-        lng: -0.012714,
-        type: 'Shopping'
-    },
-    {
-        title: 'Pizza Express',
-        lat: 51.505233,
-        lng: -0.021716,
-        type: 'Restaurant'
-    }
-]
-
-
-
-var styles = [
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#b5cbe4"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "color": "#efefef"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#83a5b0"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#bdcdd3"
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#e3eed3"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 33
-            }
-        ]
-    },
-    {
-        "featureType": "road"
-    },
-    {
-        "featureType": "poi.park",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 20
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "stylers": [
-            {
-                "lightness": 20
-            }
-        ]
-    }
-];
-
+//Location List
+var myLocations = [{
+    title: 'Truffles',
+    lat: 12.9335,
+    lng: 77.6143,
+    type: 'Restaurant'
+}, {
+    title: 'Hari Super Sandwich',
+    lat: 12.9329,
+    lng: 77.5826,
+    type: 'Restaurant'
+}, {
+    title: 'Hard Rock Cafe',
+    lat: 12.9762,
+    lng: 77.6016,
+    type: 'Restaurant'
+}, {
+    title: 'S N Refreshments',
+    lat: 12.9084,
+    lng: 77.5871,
+    type: 'Restaurant'
+}, {
+    title: 'Smoke House Deli',
+    lat: 12.9656,
+    lng: 77.6412,
+    type: 'Restaurant'
+}, {
+    title: 'Mavalli Tiffin Room (MTR)',
+    lat: 12.9552,
+    lng: 77.5855,
+    type: 'Restaurant'
+}, {
+    title: 'Chakum Chukum',
+    lat: 12.9724,
+    lng: 77.6393,
+    type: 'Restaurant'
+}, {
+    title: 'Mangalore Pearl',
+    lat: 12.9944,
+    lng: 77.6155,
+    type: 'Restaurant'
+}]
 
 // Global Variables
 var map, clientID, clientSecret;
@@ -169,9 +48,7 @@ function AppViewModel() {
     var self = this;
     this.searchOption = ko.observable("");
     this.markers = [];
-    // This function populates the infowindow when the marker is clicked. We'll only allow
-    // one infowindow which will open at the marker that is clicked, and populate based
-    // on that markers position.
+    // This function populates the infowindow when the marker is clicked.
     this.populateInfoWindow = function(marker, infowindow) {
         if (infowindow.marker != marker) {
             infowindow.setContent('');
@@ -191,17 +68,15 @@ function AppViewModel() {
                 self.street = response.location.formattedAddress[0];
                 self.city = response.location.formattedAddress[1];
                 self.zip = response.location.formattedAddress[3];
-                self.country = response.location.formattedAddress[4];
                 self.category = response.categories[0].shortName;
 
                 self.htmlContentFoursquare =
-                    '<h5 class="iw_subtitle">(' + self.category +
+                    '<h5 class="subtitle">(' + self.category +
                     ')</h5>' + '<div>' +
-                    '<h6 class="iw_address_title"> Address: </h6>' +
-                    '<p class="iw_address">' + self.street + '</p>' +
-                    '<p class="iw_address">' + self.city + '</p>' +
-                    '<p class="iw_address">' + self.zip + '</p>' +
-                    '<p class="iw_address">' + self.country +
+                    '<h6 class="address_title"> Address: </h6>' +
+                    '<p class="address">' + self.street + '</p>' +
+                    '<p class="address">' + self.city + '</p>' +
+                    '<p class="address">' + self.zip + '</p>' +
                     '</p>' + '</div>' + '</div>';
                 infowindow.setContent(self.htmlContent + self.htmlContentFoursquare);
             }).fail(function() {
@@ -210,7 +85,7 @@ function AppViewModel() {
                     "There was an issue loading the Foursquare API. Please refresh your page to try again."
                 );
             });
-            this.htmlContent = '<div>' + '<h4 class="iw_title">' + marker.title +
+            this.htmlContent = '<div>' + '<h4 class="title">' + marker.title +
                 '</h4>';
             infowindow.open(map, marker);
             infowindow.addListener('closeclick', function() {
@@ -228,19 +103,15 @@ function AppViewModel() {
     this.initMap = function() {
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
-            center: new google.maps.LatLng(51.4980479, -0.0105351),
+            center: new google.maps.LatLng(12.9716, 77.5946),
             zoom: 13,
-            styles: styles
         };
-        // Constructor creates a new map - only center and zoom are required.
         map = new google.maps.Map(mapCanvas, mapOptions);
-        // Set InfoWindow
         this.largeInfoWindow = new google.maps.InfoWindow();
         for (var i = 0; i < myLocations.length; i++) {
             this.markerTitle = myLocations[i].title;
             this.markerLat = myLocations[i].lat;
             this.markerLng = myLocations[i].lng;
-            // Google Maps marker setup
             this.marker = new google.maps.Marker({
                 map: map,
                 position: {
@@ -260,9 +131,6 @@ function AppViewModel() {
     };
 
     this.initMap();
-
-    // This block appends our locations to a list using data-bind
-    // It also serves to make the filter work
     this.myLocationsFilter = ko.computed(function() {
         var result = [];
         for (var i = 0; i < this.markers.length; i++) {
@@ -279,25 +147,19 @@ function AppViewModel() {
     }, this);
 }
 
-googleError = function googleError() {
-    alert(
-        'Oops. Google Maps did not load. Please refresh the page and try again!'
-    );
-};
-
 function initMaps() {
     ko.applyBindings(new AppViewModel());
 }
 
 $(document).ready(function() {
-  function setHeight() {
-    windowHeight = $(window).innerHeight();
-    $('#map').css('min-height', windowHeight);
-    $('#sidebar').css('min-height', windowHeight);
-  };
-  setHeight();
-
-  $(window).resize(function() {
+    function setHeight() {
+        windowHeight = $(window).innerHeight();
+        $('#map').css('min-height', windowHeight);
+        $('#sidebar').css('min-height', windowHeight);
+    };
     setHeight();
-  });
+
+    $(window).resize(function() {
+        setHeight();
+    });
 });
